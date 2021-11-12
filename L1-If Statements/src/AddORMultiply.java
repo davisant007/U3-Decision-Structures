d23334cbac5e3aa132f92990f15f4d97c9e4bc42
davisant007 <davisant007@warren.k12.in.us>
2021-11-12 12:57:27 -0500

    unit 3 L1 notes

--- L1-If Statements/src/AddORMultiply.java
+++ L1-If Statements/src/AddORMultiply.java
@@ -1,10 +1,57 @@
+import javax.swing.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class AddORMultiply {
 
     /*
     Write two methods, one that adds two numbers and second that multiplies them.
     Then, write a main method that prompts the user to enter 2 numbers.
 
-    If the first one is larger than the second, mutiply and display the result
+    If the first one is larger than the second, multiply and display the result
     If not, add them and display the results.
      */
+    public static void main(String[] args) {
+
+        double numInput1 = double.parseDouble(JOptionPane.showInputDialog("Please enter a number"));
+        double numInput2 = double.parseDouble(JOptionPane.showInputDialog("Please enter a number"));
+        double result = 0;
+
+        if(numInput1 > numInput2){
+           result = mutiply(numInput1, numInput1);
+
+        }
+        if(numInput1 <= numInput2){
+            result = add(numInput1, numInput2)
+
+
+        }
+
+       JOptionPane.showMessageDialog(null, "the result is " + result);
+
+
+
+        public static double add;(double num1 , double num2){
+
+            return num1 + num2;
+
+        }
+        public static double mutiply(double num1, double num2){
+
+        return num1 * num2;
+
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
--- L1-If Statements/src/MovieTickets.java
+++ L1-If Statements/src/MovieTickets.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MovieTickets {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Scanner read = new Scanner(System.in);
 
