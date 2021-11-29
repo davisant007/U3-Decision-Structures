b1787b8ca4f37f047bc77efc38eb91b4b042ac8d
davisant007 <davisant007@warren.k12.in.us>
2021-11-29 12:38:13 -0500

    unit 3 L3 notes

--- L3-Random Numbers/src/QuizGame.java
+++ L3-Random Numbers/src/QuizGame.java
@@ -1,2 +1,51 @@
-public class QuizGame {
+import java.util.Random;
+import javax.swing.*;
+
+
+
+public class QuizGame(){
+    JPanel panel;
+    JButton createProblem, checkAnswer;
+    JLabel showProblem , resultLabel;
+    static int userAnswer, correctAnswer;
+    static String displayProblems = "";
+        }
+
+    public static void main(String[] args) {
+createProblems();
+getUserAnswer();
+checkAnswers();
+int choice = Integer.parseInt(JOptionPane)
+
+    }
+
+    public static void checkAnswers() {
+        if (userAnswer == correctAnswer)
+            JOptionPane.showMessageDialog(null, "you are correct!");
+    }else{
+        JOptionPane.showMessageDialog(null, "incorrect answer, please try again");
+
+    }
+    public static void getUserAnswer(){
+        userAnswer = input(displayProblems);
+    }
+    public static void adProblem(){
+
+        int number1 = random();
+        int number2 = random();
+
+        correctAnswer = number1 + number2;
+        displayProblems - number1 + "" + number2;
+
+    }
+
+    //HELPER METHOD
+    public static int random(){
+
+        Random random =new Random();
+
+        return random.nextInt(20)+1;
+
+    }
+public static void createProblem()
 }
